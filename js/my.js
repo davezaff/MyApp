9765e5d91bc5a751e1583e66d03699f447513bb7
David Zaffarano <Davezaff@gmail.com>
2013-02-16 15:40:36 -0500

    ver26

--- js/my.js
+++ js/my.js
@@ -1,8 +1,20 @@
 $(document).ready(
-		function() {
-		$.mobile.loading('show');
-		$.mobile.loading('hide');
-		$('.mySearchInput').textinput();
+				function() {
+		$.mobile.loading('show',{
+		text: 'loading page..',
+	textVisible: true,
+	theme: 'z',
+	html: ""
+});
+	window.onload = function(){
+		$('#sbar').focus();
+		$('.mySearchInput').textinput();	 
 	
-	});
+		$.mobile.loading("hide");
+	};		
+			 
+});
+		
+		
+
 		
